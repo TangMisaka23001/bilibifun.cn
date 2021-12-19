09b04890028b7ac62a4b0b775859fc9b19c19786
MisakaTang <mikasatang@gmail.com>
2021-12-19 15:19:56 +0800

    update shell

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ git pull
 
 docker build -t bilibifun-home .
 
-docker stop bilibifun-home && docker rm bilibifun-home
+(docker stop bilibifun-home && docker rm bilibifun-home) || true
 
 docker run -p 9013:80 --name bilibifun-home -d bilibifun-home
