fba96c1daced2a430fdfb9226f0cfd0c680edfa5
MisakaTang <20449782+TangMisaka23001@users.noreply.github.com>
2023-05-12 15:39:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ docker build -t bilibifun-home .
 (docker stop bilibifun-home && docker rm bilibifun-home) || true
 
 docker run --name bilibifun-home -d --network traefik-public --restart=always bilibifun-home
-
-echo y | docker system prune
