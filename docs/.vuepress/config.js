62cf65ac8b99ca22c5ab42553b9b33486ade8820
MisakaTang <20449782+TangMisaka23001@users.noreply.github.com>
2023-05-17 08:44:56 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,13 +10,13 @@ module.exports = {
     //顶部导航栏
     navbar: [
       {text:'我的博客', link: 'https://misakatang.cn'},
+      {text:'Uptime-Kuma', link: 'https://uptime.bilibifun.cn'},
       {text:'Traefik', link: 'https://traefik.bilibifun.cn/dashboard/'},
       {text:'YARR', link: 'https://yarr.bilibifun.cn'},
       {text:'Portainer', link: 'https://portainer.bilibifun.cn'},
       {text:'Dozzle', link: 'https://dozzle.bilibifun.cn'},
       {text:'Plausible', link: 'https://plausible.bilibifun.cn'},
       {text:'青龙面板', link: 'https://qinglong.bilibifun.cn'},
-      // {text:'NPS', link: 'https://www.bilibifun.cn/nps'},
       {text:'Bitwarden', link: 'https://bitwarden.bilibifun.cn'},
       {text:'RSS Hub', link: 'https://rsshub.bilibifun.cn'},
       {text:'V2rayA', link: 'https://v2raya.bilibifun.cn'},
